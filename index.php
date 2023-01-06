5eb203cd90ad11e449583dfb630c91380cd98f65
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-06 17:19:58 +0200

    main

--- index.php
+++ index.php
@@ -13,15 +13,15 @@
 <form>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text"></div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
   </div>
   
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
   <div class="register">
     <p>Don't have an account? Click <a href="registration/registration.php">here</a> to register</p>
   </div>
